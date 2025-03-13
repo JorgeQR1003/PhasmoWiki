5949125db7e758ff1ae38c2c8dc731ab1f93c529
Jorge Quintero <jorgeqr@cetys.edu.mx>
2025-03-12 17:53:39 -0700

    hover ghost button

--- app/components/ghostButton.tsx
+++ app/components/ghostButton.tsx
@@ -1,5 +1,6 @@
-import { Text, View, StyleSheet, TouchableOpacity, ImageBackground } from "react-native";
+import { Text, View, StyleSheet, TouchableOpacity, Pressable } from "react-native";
 import { useColorScheme } from "react-native";
+import { useState } from "react";
 
 const styles = StyleSheet.create ({
     light_button: {
@@ -8,7 +9,11 @@ const styles = StyleSheet.create ({
         backgroundColor: "#222121",
         borderRadius: 10, 
         margin: 10, 
-        overflow: "hidden"
+        overflow: "hidden",
+        
+    },
+    light_button_hover: {
+        backgroundColor: "white",
     },
     light_buttonText: {
         marginTop: 5,
@@ -19,6 +24,10 @@ const styles = StyleSheet.create ({
         justifyContent: "center",
         textAlign: "center",
     },
+    light_buttonText_hover: {
+        color: "#222121",
+        textDecorationLine: "underline",
+    },
     dark_button: {
         width: 160, 
         height: 50, 
@@ -26,6 +35,9 @@ const styles = StyleSheet.create ({
         borderRadius: 10, 
         margin: 10, 
         overflow: "hidden"
+    },
+    dark_button_hover: {
+        backgroundColor: "white",
     },
     dark_buttonText: {
         marginTop: 5,
@@ -36,15 +48,40 @@ const styles = StyleSheet.create ({
         justifyContent: "center",
         textAlign: "center",
     },
+    dark_buttonText_hover: {
+        color: "black",
+        textDecorationLine: "underline",
+    },
 });
 
 export const BotonGhost = ({nombre = "default"}) => {
     const colorScheme = useColorScheme();
 
-    return(
-        <TouchableOpacity style={colorScheme === 'dark' ? styles.dark_button : styles.light_button} onPress={() => alert(`Button ${nombre} Pressed!`)}>
-            <Text style={colorScheme === 'dark' ? styles.dark_buttonText : styles.light_buttonText}>{nombre}</Text>
-        </TouchableOpacity>
-    )
+    const [isPressed, setIsPressed] = useState(false);
 
+    return(
+        <Pressable  
+        style={({ pressed }) => [
+            colorScheme === 'dark'
+              ? [styles.dark_button, pressed && styles.dark_button_hover]
+              : [styles.light_button, pressed && styles.light_button_hover],
+          ]}
+        onPressIn={() => setIsPressed(true)}
+        onPressOut={() => setIsPressed(false)}
+        onPress={() => alert(`Button ${nombre} Pressed!`)}>
+            <Text 
+                style={[
+                    colorScheme === 'dark'
+                      ? [ 
+                            styles.dark_buttonText, 
+                            isPressed && styles.dark_buttonText_hover 
+                        ]
+                      : [
+                          styles.light_buttonText,
+                          isPressed && styles.light_buttonText_hover,
+                        ],
+                  ]}
+            >{nombre}</Text>
+        </Pressable>
+    )
 }
